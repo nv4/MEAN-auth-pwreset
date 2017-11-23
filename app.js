4179804a062a72caa452ab251a985542937de708
Nivesh Varma <nvarma@ncsu.edu>
2017-11-22 20:45:05 -0500

    craeted POST login route

--- app.js
+++ app.js
@@ -109,6 +109,19 @@ app.get('/login', function(req, res) {
   });
 });
 
+app.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) return next(err)
+    if (!user) {
+      return res.redirect('/login')
+    }
+    req.logIn(user, function(err) {
+      if (err) return next(err);
+      return res.redirect('/');
+    });
+  })(req, res, next);
+});
+
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
