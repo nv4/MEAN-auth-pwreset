d0988f7a9a1cd4cea403aa7a30d4a39f8654a5d9
Nivesh Varma <nvarma@ncsu.edu>
2017-11-22 21:15:17 -0500

    minor cosmetic changes

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.get('/', function(req, res) {
   res.render('index', {
-    title: 'Express Test',
+    title: 'MEAN Authentication Project',
     user: req.user
   });
 });
